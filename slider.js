aced96bbebe4fd177ff6a17565e4579220c11a5f
Lova223 <andrealovati.cube3@gmail.com>
2018-04-11 09:31:58 +0100

    added izi modal library, added some more aos data to dom element

--- slider.js
+++ slider.js
@@ -1,21 +1,14 @@
 window.onload = function(){ 
-    var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
-    }
-    x[slideIndex-1].style.display = "block"; 
-}
+    $("#modal").iziModal();
+    $("#modal2").iziModal();
 
 
+    $(document).on('click', '.trigger', function (event) {
+        event.preventDefault();
+        $('#modal').iziModal('open');
+    })
+    $(document).on('click', '.trigger2', function (event) {
+        event.preventDefault();
+        $('#modal2').iziModal('open');
+    });
 }
